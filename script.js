59218fca5eee1c56d6ad59ae8e6dfefbd7526c1a
Rumana K <rumana2k20@gmail.com>
2021-07-12 08:06:03 +0600

    Analog clock created

---
+++ script.js
@@ -0,0 +1,19 @@
+// Low of clock rotation
+// Hours = (30h+m/2)
+// Minutes = 6m
+// Second = 6s
+
+
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
